747566d220630cb602aca9ea4074868d9c915b76
Jonah Janzen <jonah.janzen@gmail.com>
2025-03-03 10:08:52 -0500

    Document action internals

--- install_iscc.sh
+++ install_iscc.sh
@@ -2,9 +2,13 @@
 
 set -eEu
 
+# Start a dummy X server with display 0, and run it in the background.
+# Waiting 2 seconds is a hack to make sure it is started by the time we run the installer process.
 sudo Xdummy :0 > /dev/null 2>&1 &
-curl -SL "https://files.jrsoftware.org/is/6/innosetup-6.4.0.exe" -o is.exe
 sleep 2
+
+# Install InnoSetup 6
+curl -SL "https://files.jrsoftware.org/is/6/innosetup-6.4.0.exe" -o is.exe
 wine is.exe /SP- /VERYSILENT /ALLUSERS /SUPPRESSMSGBOXES /DOWNLOADISCRYPT=1
 rm is.exe
 
@@ -12,7 +16,7 @@ winenv() {
     wine cmd /c "echo $1" | tr -d '\r'
 }
 
+# Install unofficial languages
 PROGFILES_PATH="$(winepath -u "$(winenv %PROGRAMFILES%)")"
-
 cd "$PROGFILES_PATH/Inno Setup 6/Languages"
 curl -L "https://api.github.com/repos/jrsoftware/issrc/tarball/is-6_4_0" | tar xz --strip-components=4 --wildcards "*/Files/Languages/Unofficial/*.isl"
--- iscc.sh
+++ iscc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+set -eEu
 
 escone() {
     printf %s\\n "$1" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/' \\\\/"
@@ -22,10 +22,11 @@ winenv() {
 }
 
 PROGFILES_PATH="$(winepath -u "$(winenv %PROGRAMFILES%)")"
-
 INNO_BIN="Inno Setup 6/ISCC.exe"
 INNO_PATH="${PROGFILES_PATH}/${INNO_BIN}"
 
+# Convert all input paths to Windows-style paths so InnoSetup can parse them
 eval set -- "$(winpaths "$@")"
 
+# Run InnoSetup with the given arguments
 exec wine "$INNO_PATH" "$@"
